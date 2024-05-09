645841907a08ac834fccd0b075b93f3c40d385aa
Julius Haring <info@juliusharing.com>
2024-05-09 18:11:09 +0000

    feat: add basic supabase implementation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DancingModule } from './dancing/dancing.module';
 import { StatusController } from './status.controller';
+import { ConfigModule } from '@nestjs/config';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [AuthModule, DancingModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    DancingModule,
+    SupabaseModule,
+  ],
   controllers: [StatusController],
   providers: [],
 })
---
+++ backend/src/supabase/supabase.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+
+@Injectable()
+export class SupabaseService {
+  private supabaseClient: SupabaseClient;
+
+  constructor(private configService: ConfigService) {
+    const supabaseUrl = this.configService.getOrThrow<string>('SUPABASE_URL');
+    const supabaseKey = this.configService.getOrThrow<string>('SUPABASE_KEY');
+
+    this.supabaseClient = createClient(supabaseUrl, supabaseKey);
+  }
+}
