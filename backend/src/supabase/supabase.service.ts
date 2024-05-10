3209b4ff732b806937f7db6323785f8eb6331c7f
Julius Haring <info@juliusharing.com>
2024-05-10 10:41:10 +0000

    feat: inject request to supabase client

--- backend/src/supabase/supabase.service.ts
+++ backend/src/supabase/supabase.service.ts
@@ -1,15 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Scope } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { REQUEST } from '@nestjs/core';
+import {
+  SupabaseClient,
+  SupabaseClientOptions,
+  createClient,
+} from '@supabase/supabase-js';
+import { ExtractJwt } from 'passport-jwt';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class SupabaseService {
   private supabaseClient: SupabaseClient;
   get client(): SupabaseClient {
     return this.supabaseClient;
   }
 
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    @Inject(REQUEST) private readonly request: Request,
+  ) {
+    const clientOptions: SupabaseClientOptions<'public'> = {
+      auth: {
+        autoRefreshToken: true,
+        detectSessionInUrl: false,
+      },
+      global: {
+        headers: {
+          Authorization: `Bearer ${ExtractJwt.fromAuthHeaderAsBearerToken()(this.request)}`,
+        },
+      },
+    };
+
     const supabaseUrl = this.configService.getOrThrow<string>('SUPABASE_URL');
     const supabaseKey = this.configService.getOrThrow<string>('SUPABASE_KEY');
 
