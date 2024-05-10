7f93b7696104db3256b37d3675be22d1b3ce10f8
Julius Haring <info@juliusharing.com>
2024-05-10 10:42:49 +0000

    feat: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+  
   const app = await NestFactory.create(AppModule);
+
+  const corsOrigin = process.env['CORS_ORIGIN'];
+  logger.log(`CORS Origin: ${corsOrigin}`);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    allowedHeaders: ['content-type', 'authorization'],
+    origin: process.env['CORS_ORIGIN'],
+    credentials: true,
+  });
+
 
   const config = new DocumentBuilder()
     .setTitle('Kombo')
