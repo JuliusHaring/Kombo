b777e3acb43c14a9a558a8ed4f364217b1d16a83
Julius Haring <info@juliusharing.com>
2024-05-10 10:32:40 +0000

    feat: add middlewares

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DancingModule } from './dancing/dancing.module';
 import { StatusController } from './status.controller';
 import { ConfigModule } from '@nestjs/config';
 import { SupabaseModule } from './supabase/supabase.module';
+import { HTTPLoggingMiddleware } from './middlewares/http-logging.middleware';
+import { JsonBodyMiddleware } from './middlewares/json-body.middleware';
+import { RawBodyMiddleware } from './middlewares/raw-body.middleware';
 
 @Module({
   imports: [
@@ -15,4 +18,17 @@ import { SupabaseModule } from './supabase/supabase.module';
   controllers: [StatusController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RawBodyMiddleware)
+      .forRoutes({
+        path: 'stripe/webhook',
+        method: RequestMethod.POST,
+      })
+      .apply(JsonBodyMiddleware)
+      .forRoutes('*')
+      .apply(HTTPLoggingMiddleware)
+      .forRoutes('*');
+  }
+}
