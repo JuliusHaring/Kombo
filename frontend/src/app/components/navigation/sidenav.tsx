ae1fc00aa3a8a90fbc246e2631e662252bb13e04
Julius Haring <info@juliusharing.com>
2024-05-11 15:42:30 +0200

    feat: make whole navbar item clickable

--- frontend/src/app/components/navigation/sidenav.tsx
+++ frontend/src/app/components/navigation/sidenav.tsx
@@ -2,11 +2,10 @@ import { SideNavItem } from './sidenavitem';
 
 export default function SideNav() {
   return (
-    <div className="group fixed bottom-0 left-0 top-0 z-50 bg-beige-500 text-white">
+    <div className="group fixed bottom-0 left-0 top-0 z-50 bg-beige-500 text-white shadow-xl">
       <div className="min-h-screen w-16 overflow-hidden pt-2 transition-all duration-300 ease-in-out hover:w-64">
         <ul>
           <SideNavItem href="/" value="Home" />
-          {/* Add more SideNavItem components as needed */}
         </ul>
       </div>
     </div>
